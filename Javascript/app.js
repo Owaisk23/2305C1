221e442eb80b688ba26bb14b90572e97d515fcc8
Owais Ahmed Khan <owais1566@gmail.com>
2023-07-20 09:57:17 +0500

    js file added

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,1 +1,154 @@
-document.write("Hello In JS WOrld")
+// document.write("Hello In JS WOrld")
+
+// var fname = "Owais"; //declare & assign val
+
+// document.write(fname);
+//  var age; //declare
+// age = 23;
+
+// document.write(fname);
+// document.write(age);
+
+
+
+
+// fname = "Khan";
+// age = 33;
+ 
+// document.write(fname); //expected outcome: 
+// document.write(age);
+
+// let var const
+
+// let firstName = 'Abdullah';
+// document.write(firstName);
+// firstName = 'Owais';
+// let firstName = 'Owais';
+
+// let firstName = "Owais";
+// document.write(firstName);
+
+//PRinting values in Javascript 
+//Document.write
+
+// var fname = "owais";
+// document.write(fname);
+
+//console log
+
+// var fname = "Ashar";
+// var age = 20;
+// console.log(age);
+
+// alert
+
+// var fname = "owais";
+// var age = 33;
+// alert(age);
+
+// Arithematic Operation
+// var num1 = 20;
+// var num2 = 30;
+
+// var add = num1 + num2;
+// console.log(typeof(add));
+
+// DATA TYPES
+// var data = 23;
+
+// console.log(typeof(data));
+
+
+// var a = null;
+// console.log(a);
+// const num = 23;
+// num = 33;
+
+// var std_Name = ["Ashar", "Abdullah", "Taha"]
+
+
+//--------------------CLASS 2-------------------------------------
+
+
+// // arithmetic operators in Javascript
+
+// var num1 = 14;
+// var num2 = 2;
+
+// var sum = num1 % num2; 
+// console.log(sum);
+
+// // concatenation
+
+// var fname = "Muhammad Ali";
+// console.log("GRID assig is done by her sister: " + fname); //
+
+// var number = 2 + 2 + 'Abcd';
+// console.log(number); //expected outcome = 4Abcd, 
+
+// var number = 'Abcd' + 2 + 3;   
+// console.log(number); 
+
+// var name = 'Pakistan ' + 'Zindabad';
+// console.log(name)
+
+// Precedence
+
+// var exp = 2 + 4 * 6 / 2; 
+//             // 2 + 4 * 3
+//             // 2+ 12
+//             // 14
+// console.log('Precedence outcome: ' + exp) // expected outcome= 14   , real= 14
+
+
+// var exp1 = (2*5) - 10 / (2 * 3);
+//             //  10 - 10 / 6 
+//             // 10 - 1.66 
+// console.log(exp1) //real = 8.33
+
+// // post and pre increament and decrement
+
+// var a = 10;
+// a = ++a; // 10 +1 
+// a = ++a; // 11 + 1
+// var b = a;
+
+// console.log('Value of a is: '+ a)
+// console.log('Value of b is: '+ b)
+
+// var x = 2;
+
+// var y = x + x + x++ + x + ++x;  //x+1
+//     // 2 + 2 + 2 + 3 + 4
+// console.log('Output 0f y: '+ y); // 13
+// console.log('Output of x: '+ x); // 4
+
+
+// var x =5;
+
+// var y = x + ++x + x++ + --x + x-- + x;
+//        // 5 + 6  +  6   +  6  + 6    + 5
+
+// console.log('Outcome of x '+ x); // 5
+// console.log('Outcome of y ' + y); // 34
+
+// var a = 2
+// var b = a - a++ + --a + a-- + a;
+//        // 2 - 2  +  2  + 2   + 1
+// alert(a + ' and ' + b);   // a = 1 & b = 5
+
+
+// console.log("It is the 'red' box");
+
+// // relationaal operator 
+
+console.log('=========================')
+
+var number1 = 6; // number
+var number2 = '6'; // string
+
+
+var result = number1 !== number2;
+
+console.log(result); // true
+
