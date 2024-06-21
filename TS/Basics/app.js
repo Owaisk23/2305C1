7d9620db3e4380b29176849099d04b0b1a77cb23
Owais Ahmed Khan <owais1566@gmail.com>
2024-06-21 12:59:39 +0500

    TS basics

---
+++ TS/Basics/app.js
@@ -0,0 +1,14 @@
+console.log("Hello in Typescript");
+// alert("Eid Mubarak!!")
+var userName = "Owais Ahmed Khan";
+// // userName = true;
+// console.log(userName);
+console.log(userName.toLowerCase());
+// console.log(typeof userName);
+var userAge = 23;
+// console.log(userAge);
+// console.log(typeof userAge);
+var userEmail = "abc@gmail.com";
+var userDetails = "User email is ".concat(userEmail, " and Name is ").concat(userName, " & age is ").concat(userAge);
+// console.log(typeof userEmail);
+console.log(userDetails);
---
+++ TS/Basics/app.ts
@@ -0,0 +1,23 @@
+console.log("Hello in Typescript");
+
+// alert("Eid Mubarak!!")
+
+let userName: string = "Owais Ahmed Khan";
+// // userName = true;
+// console.log(userName);
+console.log(userName.toLowerCase());
+// console.log(typeof userName);
+
+let userAge: number = 23;
+
+// console.log(userAge);
+
+// console.log(typeof userAge);
+
+let userEmail = "abc@gmail.com";
+let userDetails = `User email is ${userEmail} and Name is ${userName} & age is ${userAge}`
+
+
+// console.log(typeof userEmail);
+console.log(userDetails)
+
