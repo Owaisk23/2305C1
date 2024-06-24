aff64cf7f075c4c875ce48f77c09333f9c384e31
Owais Ahmed Khan <owais1566@gmail.com>
2024-06-24 13:10:14 +0500

    arrays in TS

--- TS/Basics/app.js
+++ TS/Basics/app.js
@@ -1,14 +1,50 @@
 console.log("Hello in Typescript");
 // alert("Eid Mubarak!!")
-var userName = "Owais Ahmed Khan";
+// let userName: string = "Owais Ahmed Khan";
 // // userName = true;
 // console.log(userName);
-console.log(userName.toLowerCase());
+// console.log(userName.toLowerCase());
 // console.log(typeof userName);
-var userAge = 23;
+// let userAge: number = 23;
 // console.log(userAge);
 // console.log(typeof userAge);
-var userEmail = "abc@gmail.com";
-var userDetails = "User email is ".concat(userEmail, " and Name is ").concat(userName, " & age is ").concat(userAge);
+// let userEmail = "abc@gmail.com";
+// let userDetails = `User email is ${userEmail} and Name is ${userName} & age is ${userAge}`
 // console.log(typeof userEmail);
-console.log(userDetails);
+// console.log(userDetails)
+// Null
+// let userNam = null;
+// console.log(userNam);
+// console.log(typeof userNam);
+// if(userNam){
+//     alert("Null True huta hai");
+// }else{
+//     alert("Null False Huta hai");
+// }
+// // undefined
+// let userPass;
+// console.log(userPass);
+// console.log(typeof userPass);
+// if(userPass){
+//     alert("Undefined true huta hai");
+// }else{
+//     alert("Undefined false huta hai");
+// }
+// Object datatype with any
+// let products: any = {
+//     prodName: "Samsung Smart LED",
+//     prodPrice: 67000,
+//     prodDiscount: 2342
+// }
+// console.log(products);
+// console.log(products.prodName);
+// console.log(products.prodPrice);
+// Arrays in TS
+var stdMarks = [77, 88, 95, 65, 45, 100];
+console.log(stdMarks);
+console.log(stdMarks[0]);
+console.log(stdMarks[5]);
+stdMarks[2] = 56;
+console.log(stdMarks);
+// let stdNames: string[] = ["Ali", "Taha", "Azhar", "Bilal", "Ebad", "Haider"]
+// console.log(stdNames)
