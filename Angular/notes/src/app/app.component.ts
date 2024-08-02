2b03c95067fe5f6b82015d4d8eebb7f5f78cd2dd
Owais Ahmed Khan <owais1566@gmail.com>
2024-08-02 12:17:49 +0500

    update notes component

--- Angular/notes/src/app/app.component.ts
+++ Angular/notes/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NotesComponent} from './notes/notes.component'
-
+import { NotesComponent } from './notes/notes.component'
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NotesComponent],
+  imports: [
+    RouterOutlet,
+    NotesComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Angular/notes/src/app/note.service.ts
+++ Angular/notes/src/app/note.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Note } from './note';
+import { addDoc, doc, deleteDoc, Firestore, updateDoc } from 'firebase/firestore';
+import { collection } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+import { collectionData } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
 
-  constructor() { }
+  constructor(private fs:Firestore) { }
+
+  addNote(note: Note){
+    note.id = doc(collection(this.fs, 'id')).id
+    return addDoc(collection(this.fs, 'Notes'),note)
+  }
+
+  getNotes():Observable<Note[]>{
+    let notesRef = collection(this.fs, 'Notes')
+    return collectionData(notesRef, {idField: 'id'}) as Observable<Note[]>
+  }  
+
+  deleteDoc(note: Note){
+    let docRef = doc(collection(this.fs, `Notes/${note.id}`));
+
+    return deleteDoc(docRef)
+  }
+
+  updateNote(note: Note, notes: any){
+    let docRef = doc(this.fs, `Notes/${note.id}`);
+    return updateDoc(docRef, notes)
+  } 
+
+
+
+
 }
