5a63435c178e53fcf59162cc33af3980e2af7120
Owais Ahmed Khan <owais1566@gmail.com>
2024-09-20 12:56:39 +0500

    update

--- SQLSERVER/BASICS.sql
+++ SQLSERVER/BASICS.sql
@@ -240,4 +240,159 @@ AddEmp @Name='Yumna', @desig='Flutter Developer', @sal=150000, @city='Lahore', @
 
 DROP PROCEDURE SeeEmp;
 
+Select * from e
+DELETE FROM Employees where id = 1004;
+
+-- indexes 
+
+create index empname_index
+On Employees(empName,salary);
+
+select * from Employees where empName='Owais';
+
+drop index employees.empname_index;
+
+-- Triggers
+
+ -- for
+ CREATE TRIGGER AddEmp_trigger 
+ On Employees
+ for INSERT
+ as
+ BEGIN
+ print('a new employee added successfully.')
+ END;
+
+ INSERT INTO Employees(empName,designation,salary,city,deptId) VALUES
+('Hussain ','developer',78788, 'jammu',2);
+
+
+ -- after
+ ALTER TRIGGER AddEmp_trigger 
+ On Employees
+ for INSERT
+ as
+ BEGIN
+SELECT * FROM inserted;
+ END;
+
+ --Creating audit table
+ CREATE TABLE EmpLogs(
+ logid int Primary key identity(1,1),
+ ActionPerformed nvarchar(255)
+ );
+ SELECT * FROM EmpLogs;
+
+
+ SELECT * into Test from Employees;
+ SELECT * From Test;
+
+ -- Maintaining logs on tables
+
+  create TRIGGER AddEmp_trigger 
+ On Employees
+ AFTER INSERT
+ as
+ BEGIN
+DECLARE @Id int, @name varchar(50)
+SELECT @Id=id, @name=empName FROM inserted
+INSERT INTO EmpLogs VALUES(@name+' having Id = '+ CAST(@Id as varchar(6)) +' is added at '+ cast(GETDATE() as varchar(30)))
+ END;
+
+
+ -- instead of
+
+ SELECT * from Employees;
+ Alter TABLE Employees ADD emp_status int default(1) not null;
+
+
+ CREATE Trigger Soft_Delete
+ On Employees
+ INSTEAD OF DELETE
+ AS
+ BEGIN
+ DECLARE @ID int
+ SELECT @ID=id from Deleted
+ Update Employees set emp_status=0 where id=@ID
+ END;
+
+
+ DELETE from Employees where id=8;
+
+
+ -- Update Trigger
+ ALTER Trigger Update_Employees
+ On Employees
+ for Update
+ as
+ begin
+DECLARE @Id int
+DECLARE @Newname varchar(60), @Oldname varchar(60)
+DECLARE @Newsalary int,  @Oldsalary int
+DECLARE @NewDesignation varchar(60),@OldDesignation varchar(60)
+DECLARE @Newdeptid int,  @Olddeptid int
+DECLARE @Newcity varchar(60), @Oldcity varchar(60)
+DECLARE @AuditString varchar(255)
+
+SELECT * into #Temptable from inserted
+WHILE(exists (SELECT id from #Temptable))
+begin
+SELECT @Id=id, @Newname=empName, @Newsalary=salary, @NewDesignation=designation,@Newcity=city,@Newdeptid=deptId FROM #Temptable
+SELECT  @Oldname=empName, @Oldsalary=salary, @OldDesignation=designation,@Oldcity=city,@Olddeptid=deptId FROM deleted where id=@Id
+SET @AuditString =''
+SET @AuditString ='An employee having id = '+ CAST(@Id as varchar(6))+' on '+  CAST(GETDATE() as varchar(30))+' is changed '
+if(@Oldname <> @Newname)
+SET @AuditString = @AuditString + ' its name from '+@Oldname +' to '+ @Newname
+
+if(@Oldsalary <> @Newsalary)
+SET @AuditString = @AuditString + ' its salary from '+CAST(@Oldsalary as varchar(10)) +' to '+CAST(@Newsalary as varchar(10))
+
+if(@OldDesignation <> @NewDesignation)
+SET @AuditString = @AuditString + ' its designation from '+@OldDesignation +' to '+ @NewDesignation
+
+if(@Oldcity <> @Newcity)
+SET @AuditString = @AuditString + ' its city from '+@Oldcity +' to '+ @Newcity
+
+if(@Olddeptid <> @Newdeptid)
+SET @AuditString = @AuditString + ' its deptid from '+CAST(@Olddeptid as varchar(10)) +' to '+CAST(@Newdeptid as varchar(10))
+ Insert into Emplogs values(@AuditString)
+ DELETE FROM #Temptable where id=@Id
+end
+ end;
+
+
+ select * from Employees;
+ update Employees set city='karachi' where id > 4;
+ select * from Emplogs;
+
+ -- Transactions 
+ -- ddl- data definition language (create , alter, drop)
+ -- dml-data manipulation language (insert,update,delete, select)
+ -- dcl-data control languages (grant/revoke)
+ -- tcl- transaction control language. (ROLLBACK/ COMMIT)
+
+ -- ACID properties.
+ 
+-- A => Atomicity -- all the steps are fully completed or not a single step will execute.
+-- C => Consistency -- all the nodes in a network should be aware of the transaction.
+-- I => Isolation -- one transaction's info should not be known to another transaction.
+-- D => Durability -- changes after transaction should be saved.
+
+-- Integrity
+Begin Transaction
+Update Employees set empName='Khurram' where id= 1;
+Commit Transaction;-- save changes
+
+Rollback Transaction;-- wapis
+
+BEGIN TRANSACTION
+BEGIN try 
+Update Employees set empName='chakwal' where id= 1/0;
+print('Commited Successfully.')
+COMMIT TRANSACTION
+End try
+BEGIN catch
+print('Rolled back Successfully.')
+Rollback Transaction
+END catch;
 
